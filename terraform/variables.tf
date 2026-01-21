1461342aef438036462e1916f213368fd3ee6b81
L00196725 <L00196725@atu.ie>
2026-01-21 23:28:42 +0000

    Experiment 0 with no drift detected.

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "filename" {
+  type        = string
+  description = "Path to the file managed by Terraform"
+  default     = "hello.txt"
+}
+
+variable "content" {
+  type        = string
+  description = "Content written into the managed file"
+  default     = "Hello from Terraform\n"
+}
